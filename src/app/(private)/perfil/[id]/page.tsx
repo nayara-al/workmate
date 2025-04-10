07c970a74243415e3567a7352295c302ce2f8606
nayara-al <anna_nayara@hotmail.com>
2025-04-10 15:40:19 -0300

    fix: rota dinamica

--- src/app/(private)/perfil/[id]/page.tsx
+++ src/app/(private)/perfil/[id]/page.tsx
@@ -1,7 +1,9 @@
 import ProfileForm from "@/components/organism/ProfileForm";
 
-export default function Profile({ params }: { params: { id: string } }) {
-  const { id } = params;
+type tParams = Promise<{ id: string }>;
+
+export default async function Profile({ params }: { params: tParams }) {
+  const { id } = await params;
 
   return (
     <main className="flex w-full h-[calc(100vh-4rem)]">
